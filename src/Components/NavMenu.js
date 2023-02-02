699d206f7babe204c0831ffafdfff910584def65
Gabriel <gabrielcgimenes@gmail.com>
2023-02-02 14:07:39 -0800

    Adds floating menu for mobile and tablet

--- src/Components/NavMenu.js
+++ src/Components/NavMenu.js
@@ -7,8 +7,29 @@ import SocialMenu from "./SocialMenu";
 const NavMenu = () => {
   const hamburger = <FontAwesomeIcon icon={faBars} />;
 
+  function toggleMenu() {
+    const floatingMenu = document.getElementById('floatingMenu');
+    floatingMenu.classList.toggle("hidden")
+  }
+
   return (
     <div className="headerMenu">
+      <div className="mobileSize">
+        <a onClick={toggleMenu}>
+          {hamburger}
+        </a>
+      </div>
+
+      <div className="floatingMenu hidden" id="floatingMenu">
+        <ul>
+          <li><a href="#" onClick={toggleMenu}>Home</a></li>
+          <li><a href="#about" onClick={toggleMenu}>About me</a></li>
+          <li><a href="#projects" onClick={toggleMenu}>Projects</a></li>
+          <li><a href="#contact" onClick={toggleMenu}>Contact me</a></li>
+          <li><SocialMenu /></li>
+        </ul>
+      </div>
+
       <div className="tabletSize">
         <ul>
           <li>
@@ -25,9 +46,6 @@ const NavMenu = () => {
           </li>
         </ul>
         <SocialMenu />
-      </div>
-      <div className="mobileSize">
-        <a>{hamburger}</a>
       </div>
     </div>
   );
