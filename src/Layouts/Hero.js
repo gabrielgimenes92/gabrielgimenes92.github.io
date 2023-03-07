546d8adc4c48e05373b34315d6c3ef557143c803
Gabriel <gabrielcgimenes@gmail.com>
2023-03-07 12:19:05 -0800

    Rebuilding home page

--- src/Layouts/Hero.js
+++ src/Layouts/Hero.js
@@ -2,22 +2,38 @@ import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faReact, faSquareJs } from "@fortawesome/free-brands-svg-icons";
 
+import profile from "../Assets/Pictures/pesadaum-pesadao.gif";
+
 const Hero = () => {
     const reactLogo = <FontAwesomeIcon icon={faReact} />;
     const javascriptLogo = <FontAwesomeIcon icon={faSquareJs} />;
 
   return (
     <section className="home">
-        <h1>Gabriel Costa Gimenes</h1>
-        <h2>Junior frontend / mobile developer</h2>
-        <div className="skills">
-          <div>
-            <p>{javascriptLogo}</p>
-            <p>JavaScript</p>
-          </div>
-          <div>
-            <p>{reactLogo}</p>
-            <p>React & React Native</p>
+      <div className="ball" />
+      <div className="ball2" />
+      <div className="ball3" />
+
+        <div className="introduction">
+          <div className="profilePicture"/>
+          <div className="profileText">
+            <p>Hi I'm</p>
+            <p className="bigger"><spam className="light">&#123;</spam> Gabriel Gimenes <spam className="light">&#125;</spam></p>
+            <p className="bigger">Junior frontend <spam className="light">/</spam> mobile developer</p>
+            <div className="skills">
+              <div>
+                <p>{javascriptLogo}</p>
+                <p>JavaScript</p>
+              </div>
+              <div>
+                <p>{reactLogo}</p>
+                <p>React & React Native</p>
+              </div>
+              <div>
+                <p>{reactLogo}</p>
+                <p>React & React Native</p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
