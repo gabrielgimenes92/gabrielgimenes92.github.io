5fdba67816873fa2071cb4c8f5572c68c8388959
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2024-07-15 13:33:53 -0700

    Adds style to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,44 +1,62 @@
 import Link from 'next/link';
 import React from 'react';
+import styles from './NavBar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const NavBar = () => {
+  const iconLinkedin = <FontAwesomeIcon icon={faLinkedin} />;
+  const iconGithub = <FontAwesomeIcon icon={faGithub} />;
+
   return (
-    <div>
-      <h1>NavBar</h1>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/#about">About</Link>
-        </li>
-        <li>
-          <Link href="/#projects">Projects</Link>
-        </li>
+    <div className={styles.navBarWrapper}>
+      <div className={styles.navBar}>
         <ul>
           <li>
-            <Link href="/adventurebc">AdventureBC</Link>
+            <Link href="/">Home</Link>
           </li>
           <li>
-            <Link href="/billy">Billy</Link>
+            <Link href="/#about">About</Link>
           </li>
           <li>
-            <Link href="/bullet-journal">Bullet Journal</Link>
+            <Link href="/#projects">Projects</Link>
+          </li>
+          <ul className={styles.navBarSubMenu}>
+            <li>
+              <Link href="/adventurebc">AdventureBC</Link>
+            </li>
+            <li>
+              <Link href="/billy">Billy</Link>
+            </li>
+            <li>
+              <Link href="/bullet-journal">Bullet Journal</Link>
+            </li>
+            <li>
+              <Link href="/everypage">EveryPage</Link>
+            </li>
+            <li>
+              <Link href="/finance-tracker">Finance Tracker</Link>
+            </li>
+            <li>
+              <Link href="/portfolio">Portfolio</Link>
+            </li>
+            <li>
+              <Link href="/py-game">Py Game</Link>
+            </li>
+          </ul>
+        </ul>
+        <ul>
+          <li>
+            <a href="">{iconLinkedin}</a>
           </li>
           <li>
-            <Link href="/everypage">EveryPage</Link>
+            <a href="">{iconGithub}</a>
           </li>
           <li>
-            <Link href="/finance-tracker">Finance Tracker</Link>
-          </li>
-          <li>
-            <Link href="/portfolio">Portfolio</Link>
-          </li>
-          <li>
-            <Link href="/py-game">Py Game</Link>
+            <a href="">Resume</a>
           </li>
         </ul>
-      </ul>
+      </div>
     </div>
   );
 };
