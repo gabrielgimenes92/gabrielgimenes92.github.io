08262a677e1db492c5fb54c9465932971ebea7c2
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2024-07-15 13:52:31 -0700

    Adds style to Hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact, faSquareJs } from '@fortawesome/free-brands-svg-icons';
+import styles from './Hero.module.scss';
 import React from 'react';
 import Image from 'next/image';
 
@@ -8,22 +9,29 @@ const Hero = () => {
   const javascriptLogo = <FontAwesomeIcon icon={faSquareJs} />;
 
   return (
-    <section className="home">
+    <section className={styles.home}>
       <div className="ball1" />
       <div className="ball2" />
       <div className="ball3" />
-      <div className="introduction">
-        <Image src="/pictures/DSCF9483.jpg" width={500} height={500} alt="hi" />
-        <div className="profileText">
+      <div className={styles.introduction}>
+        <Image
+          src="/pictures/DSCF9483.jpg"
+          width={500}
+          height={500}
+          alt="hi"
+          className={styles.profilePicture}
+        />
+        <div className={styles.profileText}>
           <p>Hi, my name is</p>
-          <p className="biggest">
-            <spam className="light">&#123;</spam> Gabriel Gimenes{' '}
-            <spam className="light">&#125;</spam>
+          <p className={styles.biggest}>
+            <spam className={styles.light}>&#123;</spam> Gabriel Gimenes{' '}
+            <spam className={styles.light}>&#125;</spam>
           </p>
-          <p className="bigger">
-            Junior frontend <spam className="light">/</spam> mobile developer
+          <p className={styles.bigger}>
+            Junior frontend <spam className={styles.light}>/</spam> mobile
+            developer
           </p>
-          <div className="hero-skills">
+          <div className={styles.heroSkills}>
             <div>
               <p>{javascriptLogo}</p>
               <p>JavaScript</p>
