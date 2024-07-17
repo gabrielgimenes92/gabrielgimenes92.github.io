214c2dedfa9c524558799b49c287f7dff99aeaa0
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2024-07-16 17:40:21 -0700

    Improves style of Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import styles from './Footer.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faMailBulk,
+  faPhone,
+  faLocationPin,
+} from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
+  const phone = <FontAwesomeIcon icon={faPhone} />;
+  const mail = <FontAwesomeIcon icon={faMailBulk} />;
+  const pin = <FontAwesomeIcon icon={faLocationPin} />;
+  const iconLinkedin = <FontAwesomeIcon icon={faLinkedin} />;
+  const iconGithub = <FontAwesomeIcon icon={faGithub} />;
+
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <section className={styles.contact}>
+      <spam id="contact"></spam>
+      <ul className={styles.upperList}>
+        <li className={styles.mail}>
+          <a href="mailto:gabrielcgimenes@gmail.com" className="icon">
+            {mail}
+            <p>gabrielcgimenes@gmail.com</p>
+          </a>
+        </li>
+        <li className={styles.contactSocial}>
+          <ul>
+            <li>
+              <a href="">{iconLinkedin}</a>
+            </li>
+            <li>
+              <a href="">{iconGithub}</a>
+            </li>
+            <li>
+              <a href="">Resume</a>
+            </li>
+          </ul>
+        </li>
+        <li className={styles.phone}>
+          <a href="tel: 236-886-7471" className="icon">
+            {phone}
+            <p>236-886-7471</p>
+          </a>
+        </li>
+        <li className={styles.addressIcon}>
+          <p>{pin} Langley, BC</p>
+        </li>
+      </ul>
+    </section>
   );
 };
 
