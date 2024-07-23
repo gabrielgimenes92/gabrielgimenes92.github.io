f3cf1be51fa225ffec7d2a34441e66e24522ca74
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2024-07-22 17:07:04 -0700

    Changes content to match everypage project

--- src/app/projects/everypage/page.js
+++ src/app/projects/everypage/page.js
@@ -12,12 +12,17 @@ const page = () => {
     {
       id: '01',
       content:
-        'Intro Content 01 Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sit aliquid nulla dolorem minima sed ratione vel alias, dolore corrupti, doloremque odit commodi voluptates libero a, necessitatibus perferendis voluptas! Voluptates?',
+        'EveryPage is a mobile application created during post degree diploma at Langara College, serving as our final project. This endeavor replicates a professional work environment, encompassing a collaborative effort with designers, developers, and a project manager. It strictly adheres to agile methodology and its associated practices.',
     },
     {
       id: '02',
       content:
-        'Intro Content 02 Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sit aliquid nulla dolorem minima sed ratione vel alias, dolore corrupti, doloremque odit commodi voluptates libero a, necessitatibus perferendis voluptas! Voluptates?',
+        'In my dual role as a project manager and a developer within the team, I actively participated in critical aspects of the project, including design decision-making, data model creation, and the selection of the technological stack, among other responsibilities. My primary focus as a developer was on front-end, utilizing React Native, integrating the Native Base library, and conducting testing through Expo.',
+    },
+    {
+      id: '03',
+      content:
+        "While the project is no longer available online, the source code is still accessible on the project's GitHub repository. My substantial contributions were concentrated in the 'wishlist' and 'notifications' screens, as well as the development of the promotional website.",
     },
   ];
 
@@ -27,18 +32,23 @@ const page = () => {
       {
         id: '01',
         content:
-          'The Application Paragraph 01 Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sit aliquid nulla dolorem minima sed ratione vel alias, dolore corrupti, doloremque odit commodi voluptates libero a, necessitatibus perferendis voluptas! Voluptates?',
+          'EveryPage is a mobile app for book lovers who wish to keep track of their book collections at home as well as meeting other people in the nearby community to exchange books.',
+      },
+      {
+        id: '02',
+        content:
+          'The app provides an easy-to-use book inventory system allowing users to create their own virtual library where they can add custom information to help them remember the books. In addition, they can share their book collection with other nearby users and create a community where they can borrow and exchange books.',
       },
     ],
   };
 
   let textAndImage01 = {
-    title: 'First image',
+    title: 'ISBN Scanner',
     paragraphs: [
       {
         id: '01',
         content:
-          'Paragraph with image Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sit aliquid nulla dolorem minima sed ratione vel alias, dolore corrupti, doloremque odit commodi voluptates libero a, necessitatibus perferendis voluptas! Voluptates?',
+          'Users can easily add books to their collection by scanning the ISBN barcode and adding manually. Once the book is added, users can personalise the book information, such as personal notes, photos and additional information to help them to organise the books.',
       },
     ],
     image: {
@@ -48,12 +58,12 @@ const page = () => {
   };
 
   let textAndImage02 = {
-    title: 'First image',
+    title: 'Virtual Bookshelf On-The-Go',
     paragraphs: [
       {
         id: '01',
         content:
-          'Paragraph with image Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sit aliquid nulla dolorem minima sed ratione vel alias, dolore corrupti, doloremque odit commodi voluptates libero a, necessitatibus perferendis voluptas! Voluptates?',
+          'Users can access their virtual bookshelf anytime and anywhere. They can also keep track of the reading status, who borrow their books, or where they keep them. Users won’t need to worry if they buy duplicate copies or lose the books.',
       },
     ],
     image: {
@@ -63,12 +73,12 @@ const page = () => {
   };
 
   let textAndImage03 = {
-    title: 'First image',
+    title: 'Discover Books',
     paragraphs: [
       {
         id: '01',
         content:
-          'Paragraph with image Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sit aliquid nulla dolorem minima sed ratione vel alias, dolore corrupti, doloremque odit commodi voluptates libero a, necessitatibus perferendis voluptas! Voluptates?',
+          'Users can share their books with other users by selecting the books they’re willing to share and tagging them as shareable. They can search for available books on the Discover page by book title or author, and nearby location. Users can check on the details of the book, add to their wishlist, and request to borrow the book.',
       },
     ],
     image: {
@@ -79,9 +89,8 @@ const page = () => {
 
   return (
     <div>
-      <h1>This is Everypage</h1>
       <SinglePageHero title="Everypage" />
-      <Paragraph content={introContent} />
+      <Paragraph paragraphs={introContent} />
       <TechStack />
       <Paragraph
         title={theApplicationParagraph.title}
--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -1,16 +1,19 @@
 //Component to create a paragraph, can have a title or not, h1 or h2.
 
 import React from 'react';
+import styles from './Paragraph.module.scss';
 
 const Paragraph = ({ title, paragraphs }) => {
   return (
-    <div>
-      {title ? <h1>{title}</h1> : <></>}
-      {paragraphs ? (
-        paragraphs.map((item) => <p id={item.id}>{item.content}</p>)
-      ) : (
-        <></>
-      )}
+    <div className={styles.paragraphWrapper}>
+      <div className={styles.paragraph}>
+        {title ? <h1>{title}</h1> : <></>}
+        {paragraphs ? (
+          paragraphs.map((item) => <p id={item.id}>{item.content}</p>)
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 };
