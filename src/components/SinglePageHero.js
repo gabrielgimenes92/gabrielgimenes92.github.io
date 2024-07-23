857c13b0b1dcc4b1acafc232a5b50e1a9f7b1134
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2024-07-23 16:10:42 -0700

    Improvements on the single project hero style

--- src/components/SinglePageHero.js
+++ src/components/SinglePageHero.js
@@ -4,6 +4,9 @@ import styles from './SinglePageHero.module.scss';
 const SinglePageHero = ({ title, subtitle }) => {
   return (
     <div className={styles.singlePageHero}>
+      <div className={styles.ball1} />
+      <div className={styles.ball2} />
+      <div className={styles.ball3} />
       <h1>{title}</h1>
       <h2>{subtitle}</h2>
     </div>
