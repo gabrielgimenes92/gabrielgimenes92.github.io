59722639cf97d1f76e4fab0b66fc01acbbffaec9
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2024-08-01 18:09:54 -0700

    Adds floating menu for the header on mobile view

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,42 +1,52 @@
+'use client';
+
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './NavBar.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+// import resume from '/pdfs/Resume.pdf';
 
 const NavBar = () => {
   const iconLinkedin = <FontAwesomeIcon icon={faLinkedin} />;
   const iconGithub = <FontAwesomeIcon icon={faGithub} />;
   const hamburger = <FontAwesomeIcon icon={faBars} />;
+  const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
+
+  const handleClick = () => {
+    setShowHamburgerMenu(!showHamburgerMenu);
+    console.log(showHamburgerMenu);
+  };
 
   return (
     <div className={styles.navBarWrapper}>
       <div className={styles.navBar}>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/#about">About</Link>
-          </li>
-          <li>
-            <Link href="/#projects">Projects</Link>
-          </li>
-          <ul className={styles.navBarSubMenu}>
+        <div className={styles.desktopSizeHeader}>
+          <ul>
             <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/#about">About</Link>
+            </li>
+            <li>
+              <Link href="/#projects">Projects</Link>
+            </li>
+            <ul className={styles.navBarSubMenu}>
+              {/* <li>
               <Link href="/adventurebc">AdventureBC</Link>
-            </li>
-            <li>
-              <Link href="/billy">Billy</Link>
-            </li>
-            <li>
+            </li> */}
+              <li>
+                <Link href="/billy">Billy</Link>
+              </li>
+              {/* <li>
               <Link href="/bullet-journal">Bullet Journal</Link>
-            </li>
-            <li>
-              <Link href="/everypage">EveryPage</Link>
-            </li>
-            <li>
+            </li> */}
+              <li>
+                <Link href="/everypage">EveryPage</Link>
+              </li>
+              {/* <li>
               <Link href="/finance-tracker">Finance Tracker</Link>
             </li>
             <li>
@@ -44,38 +54,97 @@ const NavBar = () => {
             </li>
             <li>
               <Link href="/py-game">Py Game</Link>
+            </li> */}
+            </ul>
+          </ul>
+          <ul>
+            <li>
+              <a
+                className={styles.icon}
+                href="https://www.linkedin.com/in/gabrielcgimenes/"
+                target={'blank'}
+              >
+                {iconLinkedin}
+                <spam className="hidden">LinkedIn</spam>
+              </a>{' '}
+            </li>
+            <li>
+              <a
+                className={styles.icon}
+                href="https://github.com/gabrielgimenes92"
+                target={'blank'}
+              >
+                {iconGithub}
+                <spam className="hidden">GitHub</spam>
+              </a>
+            </li>
+            <li>
+              <a className={styles.resumeDownload} target={'blank'} href="">
+                Resume
+              </a>
             </li>
           </ul>
-        </ul>
-        <ul className={styles.desktopSizeHeader}>
-          <li>
-            <a
-              className={styles.icon}
-              href="https://www.linkedin.com/in/gabrielcgimenes/"
-              target={'blank'}
-            >
-              {iconLinkedin}
-              <spam className="hidden">LinkedIn</spam>
-            </a>{' '}
-          </li>
-          <li>
-            <a
-              className={styles.icon}
-              href="https://github.com/gabrielgimenes92"
-              target={'blank'}
-            >
-              {iconGithub}
-              <spam className="hidden">GitHub</spam>
-            </a>
-          </li>
-          <li>
-            <a className={styles.resumeDownload} target={'blank'} href="">
-              Resume
-            </a>
-          </li>
-        </ul>
-        <a className={styles.tabletSizeHeader}>{hamburger}</a>
+        </div>
+        <a className={styles.tabletSizeHeader} onClick={handleClick}>
+          {hamburger}
+        </a>
       </div>
+      {showHamburgerMenu ? (
+        <div className={styles.floatingMenu}>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/#about">About</Link>
+            </li>
+            <li>
+              <Link href="/#projects">Projects</Link>
+            </li>
+            {/* <ul className={styles.navBarSubMenu}>
+              <li>
+                <Link href="/billy">Billy</Link>
+              </li>
+              <li>
+                <Link href="/everypage">EveryPage</Link>
+              </li>
+            </ul> */}
+          </ul>
+          <ul className={styles.socialFloat}>
+            <li>
+              <a
+                className={styles.icon}
+                href="https://www.linkedin.com/in/gabrielcgimenes/"
+                target={'blank'}
+              >
+                {iconLinkedin}
+                <spam className="hidden">LinkedIn</spam>
+              </a>{' '}
+            </li>
+            <li>
+              <a
+                className={styles.icon}
+                href="https://github.com/gabrielgimenes92"
+                target={'blank'}
+              >
+                {iconGithub}
+                <spam className="hidden">GitHub</spam>
+              </a>
+            </li>
+            <li>
+              <a
+                className={styles.resumeDownload}
+                target={'blank'}
+                href="/pdfs/Resume.pdf"
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
