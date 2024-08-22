ec5c4304166f9c5e150b9e8ae11b7d7f880457a1
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2024-08-22 14:48:44 -0700

    Adds to the paragraph component the ability to passa a JSX paragraph instead of just an object

--- src/app/projects/py-game/page.js
+++ src/app/projects/py-game/page.js
@@ -36,26 +36,26 @@ const page = () => {
     },
   ];
 
-  let projectOne = {
-    title: 'Title',
-    paragraphs: [
-      {
-        id: '01',
-        content:
-          'Task list is a web app designed to help users track and manage their daily tasks. It provides a straightforward interface for adding, editing, completing, and deleting tasks. The list is persistent as the application has a frontend created on React, a backend on NodeJS with Express, and a database on MongoDB.',
-      },
-      {
-        id: '02',
-        content:
-          'The app was developed so I could display some of my skills as a full-stack developer. Both front and backend are hosted and live, the former hosted on GitHub pages and the latter on Render.',
-      },
-      {
-        id: '03',
-        content:
-          'As the next step, I plan to add edit functionality and the ability to reorder tasks from the list.',
-      },
-    ],
-  };
+  // let projectOne = {
+  //   title: 'Title',
+  //   paragraphs: [
+  //     {
+  //       id: '01',
+  //       content:
+  //         'Task list is a web app designed to help users track and manage their daily tasks. It provides a straightforward interface for adding, editing, completing, and deleting tasks. The list is persistent as the application has a frontend created on React, a backend on NodeJS with Express, and a database on MongoDB.',
+  //     },
+  //     {
+  //       id: '02',
+  //       content:
+  //         'The app was developed so I could display some of my skills as a full-stack developer. Both front and backend are hosted and live, the former hosted on GitHub pages and the latter on Render.',
+  //     },
+  //     {
+  //       id: '03',
+  //       content:
+  //         'As the next step, I plan to add edit functionality and the ability to reorder tasks from the list.',
+  //     },
+  //   ],
+  // };
 
   let projectTwo = {
     title: 'Title',
@@ -78,6 +78,52 @@ const page = () => {
     ],
   };
 
+  let projectOneTitle = 'Title1';
+
+  let projectOneParagraphsJSX = (
+    <>
+      <p>
+        1Task list is a web app designed to help users track and manage their
+        daily tasks. It provides a straightforward interface for adding,
+        editing, completing, and deleting tasks. The list is persistent as the
+        application has a frontend created on React, a backend on NodeJS with
+        Express, and a database on MongoDB.
+      </p>
+      <p>
+        The app was developed so I could display some of my skills as a
+        full-stack developer. Both front and backend are hosted and live, the
+        former hosted on GitHub pages and the latter on Render.
+      </p>
+      <p>
+        As the next step, I plan to add edit functionality and the ability to
+        reorder tasks from the list.
+      </p>
+    </>
+  );
+
+  let projectTwoTitle = 'Title2';
+
+  let projectTwoParagraphsJSX = (
+    <>
+      <p>
+        2Task list is a web app designed to help users track and manage their
+        daily tasks. It provides a straightforward interface for adding,
+        editing, completing, and deleting tasks. The list is persistent as the
+        application has a frontend created on React, a backend on NodeJS with
+        Express, and a database on MongoDB.
+      </p>
+      <p>
+        The app was developed so I could display some of my skills as a
+        full-stack developer. Both front and backend are hosted and live, the
+        former hosted on GitHub pages and the latter on Render.
+      </p>
+      <p>
+        As the next step, I plan to add edit functionality and the ability to
+        reorder tasks from the list.
+      </p>
+    </>
+  );
+
   return (
     <div>
       <SinglePageHero
@@ -86,7 +132,10 @@ const page = () => {
         techStack={techStack}
         stackWidth={'300px'}
       />
-      <Paragraph paragraphs={projectOne.paragraphs} title={projectOne.title} />
+      <Paragraph
+        title={projectOneTitle}
+        paragraphsJSX={projectOneParagraphsJSX}
+      />
       <Buttons buttons={buttonFirstProject} />
       <div className={styles.imageWrapper}>
         <Image
@@ -99,7 +148,10 @@ const page = () => {
         />
       </div>
 
-      <Paragraph paragraphs={projectTwo.paragraphs} title={projectTwo.title} />
+      <Paragraph
+        title={projectTwoTitle}
+        paragraphsJSX={projectTwoParagraphsJSX}
+      />
       <Buttons buttons={buttonSecondProject} />
       <div className={styles.imageWrapper}>
         <Image
--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -3,11 +3,12 @@
 import React from 'react';
 import styles from './Paragraph.module.scss';
 
-const Paragraph = ({ title, paragraphs }) => {
+const Paragraph = ({ title, paragraphs, paragraphsJSX }) => {
   return (
     <div className={styles.paragraphWrapper}>
       <div className={styles.paragraph}>
         {title ? <h2 style={{ marginTop: '1rem' }}>{title}</h2> : <></>}
+        {paragraphsJSX}
         {paragraphs ? (
           paragraphs.map((item) => (
             <p id={item.id} key={item.id}>
